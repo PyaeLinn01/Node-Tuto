a8540bac3343943781d077645dae64eae0564e3e
PyaeLinn01 <150521896+PyaeLinn01@users.noreply.github.com>
2023-11-17 11:11:37 +0630

    readFileSync , writeFileSync, appendFileSync

--- FileSynchro.js
+++ FileSynchro.js
@@ -1,4 +1,9 @@
 let fs = require('fs');
 let data = "Playing guitar can be an extremely satisfying and rewarding."
             
-fs.writeFileSync("test.txt", data);
+//fs.writeFileSync("test.txt", data);
+fs.readFileSync('test.txt','utf-8');
+console.log(data);
+
+
+fs.writeFileSync('mine.txt',data);
---
+++ mineTotest.js
@@ -0,0 +1,5 @@
+let fs = require('fs');
+//let val = fs.readFileSync('mine.txt','utf-8');
+
+//fs.writeFileSync('test.txt',val);
+fs.appendFileSync('test.txt',"John Wick");
